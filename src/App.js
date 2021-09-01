3a62886118743644f59f5186ff12ee86ec9c152b
WAIDI ALIU <aliuwaidi@gmail.com>
2021-08-31 23:59:29 -0500

    updated login and register

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
  import Home from './Pages/Home/Home';
 // import Profile from './Pages/Profile/Profile';
 // import Login from "./Pages/Login/Login";
-//import Register from "./Pages/Register/Register";
+// import Register from "./Pages/Register/Register";
 
 function App() {
   return (
